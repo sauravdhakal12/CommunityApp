1bf0aec2b19eef91f38c8e6314da54195b4e2c57
sauravdhakal12 <sauravdhakal121@gmail.com>
2024-09-26 21:51:29 +0545

    Join a community and return info on community

--- src/api/community/community.routes.ts
+++ src/api/community/community.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { createCommunityHandler } from "@/api/community/community.controllers";
+import { createCommunityHandler, joinCommunityHandler, returnCommunityHandler } from "@/api/community/community.controllers";
 import authCheck from "@/middleware/authCheck";
 
 export const communityRouter = express.Router();
 
 communityRouter.post("/", authCheck, createCommunityHandler);
+communityRouter.get("/:id", returnCommunityHandler);
+communityRouter.post("/join", authCheck, joinCommunityHandler);
